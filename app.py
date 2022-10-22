513f06fd7313d4d86e21c413dccde6988527327a
unknown <vilazacaceres@gmail.com>
2022-10-22 18:45:43 -0300

    app y models terminado

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+from crypt import methods
+import re
+from wsgiref.util import request_uri
 from flask import Flask, request, jsonify
 from flask_migrate import Migrate
-
+from models import db, Usuario, Producto, Venta, Detalle_venta
 from flask_cors import CORS, cross_origin
 
 
@@ -16,3 +19,147 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db.init_app(app)
 
 Migrate(app, db)
+
+#RUTAS
+
+#Consultar todo
+@app.route('/usuario', methods=['GET'])
+def getUsuario():
+    usuario = Usuario.query.all()
+    usuario = list(map(lambda x: x.serialize(), usuario))
+    return jsonify(usuario), 200
+
+#consultar usuario según id
+@app.route('/usuario/<id>', methods=['GET'])
+def getUsuario(id):
+    usuario = Usuario.query.get(id)
+    return jsonify(usuario.serialize()), 200
+
+#borrar usuario según id
+@app.route('/usuario/<id>', methods=['DELETE'])
+def deleteUsuario(id):
+    usuario = Usuario.query.get(id)
+    Usuario.delete(usuario)
+    return jsonify(usuario.serialize()), 200
+
+#modificar usuario
+@app.route('/usuario/<id>', methods=['PUT'])
+def updateUsuario(id):
+    usuario = Usuario.query.get(id)
+
+    rut = request.json.get('rut')
+    nombre = request.json.get('nombre')
+    apellido = request.json.get('apellido')
+    direccion = request.json.get('direccion')
+    telefono = request.json.get('telefono')
+    correo = request.json.get('correo')
+    contrasenia = request.json.get('contrasenia')
+
+    usuario.rut = rut
+    usuario.nombre = nombre
+    usuario.apellido = apellido
+    usuario.direccion = direccion
+    usuario.telefono = telefono
+    usuario.correo = correo
+    usuario.contrasenia = contrasenia
+    Usuario.save(usuario)
+
+    return jsonify(usuario.serialize()), 200
+
+#agregar usuario
+@app.route('/usuario', methods = ['POST'])
+def addUsuario(): 
+    usuario = Usuario()
+    rut = request.json.get('rut')
+    nombre = request.json.get('nombre')
+    apellido = request.json.get('apellido')
+    direccion = request.json.get('direccion')
+    telefono = request.json.get('telefono')
+    correo = request.json.get('correo')
+    contrasenia = request.json.get('contrasenia')
+
+    usuario.rut = rut
+    usuario.nombre = nombre
+    usuario.apellido = apellido
+    usuario.direccion = direccion
+    usuario.telefono = telefono
+    usuario.correo = correo
+    usuario.contrasenia = contrasenia
+    Usuario.save(usuario)
+
+    return jsonify(usuario.serialize()), 200
+
+# 3. Creamos una ruta y debug=true para que el servidor se reinicie ante los cambios
+# 4. añadimos un validador para saber si estamos ejecutando nuestra aplicacion
+# 5. ejecutamos python app.py o python3 app.py
+# 6. ejecutamos el comando flask db init
+# 7. ejecutamos el comando flask db migrate
+# 8. ejecutamos el comando flask db upgrade
+# 9. ejecutamos el comando flask run --host=0.0.0.0 en caso que tengamos problemas con el cors
+
+#consulta productos
+@app.route('/productos', metohds = ['GET'])
+def getProductos():
+    producto = Producto.query.all()
+    producto = list(map(lambda x: x.serialize(), producto))
+    return jsonify(producto), 200
+
+#consulta por un producto
+@app.route('/productos/<id>', methods = ['GET'])
+def getProducto(id):
+    producto = Producto.query.get(id)
+    return jsonify(producto.serialize()), 200
+
+#borrar producto por id
+@app.route('/productos/<id>', methods = ['DELETE'])
+def deleteProducto(id):
+    producto = Producto.query.get(id)
+    Producto.delete(producto)
+    return jsonify(producto.serialize()), 200
+
+#modificar producto
+@app.route('/productos/<id>', methods = ['PUT'])
+def updateProducto(id):
+    producto = Producto.query.get(id)
+
+    codigo = request.json.get('codigo')
+    nombre = request.json.get('nombre')
+    valor = request.json.get('valor')
+    stock = request.json.get('stock')
+    descripcion = request.json.get('descripcion')
+    imagen = request.json.get('imagen')
+
+    producto.codigo = codigo
+    producto.nombre = nombre
+    producto.valor = valor
+    producto.stock = stock
+    producto.descripcion = descripcion
+    producto.imagen = imagen
+    Producto.save(producto)
+
+    return jsonify(producto.serialize()),200
+
+@app.route('/productos', methods = ['POST'])
+def addProducto():
+    producto = Producto.query.get(id)
+
+    codigo = request.json.get('codigo')
+    nombre = request.json.get('nombre')
+    valor = request.json.get('valor')
+    stock = request.json.get('stock')
+    descripcion = request.json.get('descripcion')
+    imagen = request.json.get('imagen')
+
+    producto.codigo = codigo
+    producto.nombre = nombre
+    producto.valor = valor
+    producto.stock = stock
+    producto.descripcion = descripcion
+    producto.imagen = imagen
+    Producto.save(producto)
+
+    return jsonify(producto.serialize()),200
+
+    if __name__ == '__main__':
+        app.run(port=3000, debug = True)
+
--- models.py
+++ models.py
@@ -1,3 +1,96 @@
+import re
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+class Region(db.Model):
+    __tablename__ ='Region'
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(250), nullable = False)
+
+
+class Comuna(db.Model):
+    __tablename__ = 'Comuna'
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(250), nullable = False)
+
+class Usuario(db.Model):
+    __tablename__ = 'Usuario'
+    id = db.Column(db.Integer, primary_key = True)
+    rut = db.Column(db.Integer, unique = True, nullable = False)
+    nombre = db.Column(db.String(250), nullable= False)
+    apellido = db.Column(db.String(250), nullable = False)
+    direccion = db.Column(db.string(250), nullable = False)
+    telefono = db.Column(db.Integer, nullable = False)
+    correo = db.Column(db.String(250), nullable = False)
+    contrasenia = db.Column(db.String(250), nullable = False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "rut": self.rut,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "direccion": self.direccion,
+            "telefono": self.telefono,
+            "correo": self.correo,
+            "contrasenia": self.contrasenia
+        }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete
+        db.session.commit()
+
+class Producto(db.Model):
+    __tablename__= 'Producto'
+    id = db.Column(db.Integer, primary_key = True)
+    codigo = db.Column(db.Integer, nullable = False)
+    nombre = db.Column(db.String(250), nullable = False)
+    valor = db.Column(db.Integer, nullable = False)
+    stock = db.Column(db.Integer, nullable = False)
+    descripcion = db.Column(db.String(250), nullable = False)
+    imagen = db.Column(db.String(250), nullable = True)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "codigo": self.codigo,
+            "nombre": self.nombre,
+            "stock": self.stock,
+            "descripcion": self.descripcion,
+            "imagen": self.imagen
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Detalle_venta(db.Model):
+    __tablename__ = 'Detalle_venta'
+    id = db.Column(db.Integer, primary_key = True)
+    cantidad = db.Column(db.Integer, nullable = False)
+    valor = db.Column(db.Integer, nullable = False)
+    descuento = db.Column(db.Integer, nullable = True)
+
+class Venta(db.Model):
+    __tablename__ = 'Venta'
+    id = db.Column(db.Integer, primary_key = True)
+    fecha = db.Column(db.DateTime, nullable = False)
+    descuento = db.Column(db.Integer, nullable = True)
+    sub_total = db.Column(db.Integer, nullable = False)
+    iva = db.Column(db.Integer, nullable = False)
+    total = db.Column(db.Integer, nullable = False)
 
